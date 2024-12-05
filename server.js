4e8c4649ecf36f074e6001b9dec52e5b00e90ab0
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-12-05 17:18:21 +0200

    test2

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // If you're sending cookies or using credentials
   })
 );
 app.use(express.json());
