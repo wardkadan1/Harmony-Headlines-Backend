94f5b360fe27e6d178ecf84df0d5fa7adfe46191
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-12-05 16:56:34 +0200

    test2

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
